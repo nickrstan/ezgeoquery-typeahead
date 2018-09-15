695b7c0aeccbb36421c1f6ec093be9ee00344ec6
Stanley <iforgotbetter@gmail.com>
2018-09-14 23:36:43 -0700

    Configuration, ignores and license

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # :facepunch: EZ Geo Query - Twitter Typeahead
 
+Ez Ego Query is a API #BRENDEN
+
+## Typeahead
+
+
+
+What i need this to do
+Combine the typahead lib and our boiler plate on top to have a public api for a typeahead 
+
 EZ Geo Query Twitter Typeahead integration example
+
+Drop in Bootstrap typeahead for searching Zip to City and City -> Zip
+
+Exmaple: 
 
 TODO
 - [ ] Finish this readme
@@ -12,6 +25,10 @@ TODO
 - [ ] Dependencies
 - [ ] jQuery slim will not work. You need support for $ajax
 - [ ] Browser support
-- [ ] Development, how to 
+- [ ] Development, how to
+- [ ] Maye gif
+
 
 [emoji](https://www.webpagefx.com/tools/emoji-cheat-sheet/)
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,31 @@
 const webpack = require('webpack');
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const nodeEnv = process.env.NODE_ENV || 'development';
 const isProd = nodeEnv === 'production';
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const plugins = [
+  new CleanWebpackPlugin(['dist']),
   new webpack.DefinePlugin({
     'process.env': {
       NODE_ENV: JSON.stringify(nodeEnv)
     }
   }),
-  new HtmlWebpackPlugin({
-    title: 'Typescript Webpack Starter',
-    template: '!!ejs-loader!src/index.html'
-  }),
-  new webpack.LoaderOptionsPlugin({
-    options: {
-      tslint: {
-        emitErrors: true,
-        failOnHint: true
-      }
-    }
-  })
 ];
 
 var config = {
   devtool: isProd ? 'hidden-source-map' : 'source-map',
+  devtool: 'inline-source-map',
   context: path.resolve('./src'),
   entry: {
-    app: './index.ts'
+    'app': './index.ts'
   },
   output: {
     path: path.resolve('./dist'),
-    filename: '[name].bundle.js'
+    filename: 'ezgeoquery-typeahead.js',
+    library: 'EzGeoTypeahead',
+    libraryTarget: "umd",
+    umdNamedDefine: true
   },
   module: {
     rules: [
@@ -42,17 +35,6 @@ var config = {
         exclude: ['node_modules'],
         use: ['awesome-typescript-loader', 'source-map-loader']
       },
-      !isProd
-        ? {
-            test: /\.(js|ts)$/,
-            loader: 'istanbul-instrumenter-loader',
-            exclude: [/\/node_modules\//],
-            query: {
-              esModules: true
-            }
-          }
-        : null,
-      { test: /\.html$/, loader: 'html-loader' },
       { test: /\.css$/, loaders: ['style-loader', 'css-loader'] }
     ].filter(Boolean)
   },
@@ -64,7 +46,6 @@ var config = {
     contentBase: path.join(__dirname, 'dist/'),
     compress: true,
     port: 3000,
-    hot: true
   }
 };
 
